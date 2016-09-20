1c424e11222691b1801e19c7b6080aa111225e8f
Thomas Hilaire <thilaire@linagora.com>
2016-09-20 18:14:10 +0200

    #12 Make params as integers

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,9 @@ program.command("import <papiUrl> <domainUuid>")
     .option("-u, --papi_user <user>", "The PAPI user login [admin0@global.virt]", "admin0@global.virt")
     .option("-p, --papi_pwd <password>", "The PAPI user password [admin]", "admin")
     .option("-h, --amqp_host <host>", "The AMQP server address [amqp://localhost]", "amqp://localhost")
-    .option("-s, --batch_size <size>", "Maximum item (e.g. event) count in one batch [5]", 5)
-    .option("-w, --batch_wait <wait>", "Time to wait (in ms) for new message from the message-queue before making a batch [5000]", 5000)
-    .option("-d, --batch_delay <delay>", "Minimal time (in ms) to wait between two batches, can be used to avoid PAPI overload [1000]", 1000)
+    .option("-s, --batch_size <size>", "Maximum item (e.g. event) count in one batch [5]", arg => parseInt(arg, 10), 5)
+    .option("-w, --batch_wait <wait>", "Time to wait (in ms) for new message from the message-queue before making a batch [5000]", arg => parseInt(arg, 10), 5000)
+    .option("-d, --batch_delay <delay>", "Minimal time (in ms) to wait between two batches, can be used to avoid PAPI overload [1000]", arg => parseInt(arg, 10), 1000)
     .action((papiUrl, domainUuid, options) => {
         ImporterFactory.create(papiUrl, domainUuid, options).importAllEvents();
     });
